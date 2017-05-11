6d992e12628ecfbb922771d0b103187dc1126cf1
Fritz <fre.healy@gmail.com>
2017-05-11 14:45:44 -0400

    adding testimonials page

--- page-templates/page-testimonials.php
+++ page-templates/page-testimonials.php
@@ -10,7 +10,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="primary" class="site-content template-testimonials">
 		<div id="content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -22,13 +22,27 @@ get_header(); ?>
 					<?php the_content(); ?>
 				</div><!-- .entry-content -->
 				<?php $args = array(
-
+					'post_type'=>'testimonial',
+					'posts_per_page'=>-1
 				);
 				$query = new WP_Query( $args );
 				if($query->have_posts()):?>
 					<div id="container">
 						<?php while($query->have_posts()):$query->the_post();?>
-							
+							<?php $tesimonial = get_field('testimonial'); ?>
+							<?php $test_cite = get_field('testimonial_credits'); ?>
+							<?php if($tesimonial):?>
+								<div class="item">
+									<div class="inner-wrapper">
+										<?php echo $tesimonial;?>
+										<?php if($test_cite):?>
+											<div class="testi-cite">
+												- <?php echo $test_cite;?>
+											</div><!-- testi cite -->
+										<?php endif;?>
+									</div><!--.wrapper-->
+								</div><!--.item-->
+							<?php endif;?>
 						<?php endwhile;?>
 					</div><!--#container-->
 				<?php endif;?>

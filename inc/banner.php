30fd48ce55b6db2bca1b09f6b07b1e651de17829
Fritz <fre.healy@gmail.com>
2018-01-25 10:35:40 -0500

    misc updates

--- inc/banner.php
+++ inc/banner.php
@@ -13,7 +13,9 @@ if ( $wp_query->have_posts() ) : ?>
 	$banner_2_text  = get_field( "banner_2_text" );
 	$banner_3_image = get_field( "banner_3_image" );
 	$banner_3_text_row_2  = get_field( "banner_3_text_row_2" );
-	$banner_3_text_row_3  = get_field( "banner_3_text_row_3" ); ?>
+	$banner_3_text_row_3  = get_field( "banner_3_text_row_3" );
+    $banner_4_image = get_field("banner_4_image");
+    $banner_4_link = get_field("banner_4_link"); ?>
     <div class="flexslider">
         <ul class="slides">
 			<?php if ( $banner_1_image ): ?>
@@ -28,14 +30,27 @@ if ( $wp_query->have_posts() ) : ?>
                     </div><!--.inner-wrapper-->
                 </li>
 			<?php endif; ?>
+			<?php if ( $banner_4_image ): ?>
+                <li class="banner-4 slide" style="background-image:url(<?php echo $banner_4_image['url']; ?>);">
+                    <?php if($banner_4_link):?>
+                        <a href="<?php echo $banner_4_link;?>">
+                    <?php endif;?>    
+                        <div class="inner-wrapper">
+                            <img src="<?php echo $banner_4_image['url']; ?>" alt="<?php $banner_4_image['alt']; ?>">
+                        </div><!--.inner-wrapper-->
+                    <?php if($banner_4_link):?>
+                        </a>
+                    <?php endif;?>    
+                </li>
+			<?php endif; ?>
 			<?php if ( $banner_2_image ): ?>
                 <li class="banner-2 slide" style="background-image:url(<?php echo $banner_2_image['url']; ?>);">
                     <div class="inner-wrapper">
                         <img src="<?php echo $banner_2_image['url']; ?>" alt="<?php $banner_2_image['alt']; ?>">
                         <div class="text">
                             <img src="<?php echo get_template_directory_uri()."/images/award-1.png" ?>" alt="Inc. 5000 Award">
-                            <img src="<?php echo get_template_directory_uri()."/images/award-2.png" ?>" alt="Fast 50 Award">
                             <img src="<?php echo get_template_directory_uri()."/images/award-3.png" ?>" alt="Fastest Growing Firms Award">
+                            <img src="<?php echo get_template_directory_uri()."/images/award-2.png" ?>" alt="Fast 50 Award">
                         </div><!--.text-->
                     </div><!--.inner-wrapper-->
                 </li>
@@ -56,6 +71,7 @@ if ( $wp_query->have_posts() ) : ?>
 						<?php endif; ?>
                         <?php if ( $banner_3_text_row_3 ): ?>
                             <div class="row-3">
+                                <img src="<?php echo get_template_directory_uri()."/images/tco-logo-2.png";?>" alt="TCO-TMPESM Logo"></br>
 								<?php echo $banner_3_text_row_3; ?>
                             </div><!--.row-3-->
 						<?php endif; ?>
